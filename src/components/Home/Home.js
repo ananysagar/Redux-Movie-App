cf0d666d1444fd3619176be3b577184cac3342f8
Sagar <anany.sagar@accenture.com>
2023-10-02 19:05:27 +0530

    API Calls

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/movieApiKey"
 function Home() {
+  useEffect(() => {
+    const movieText="Harry";
+    const fetchMovies = async() => {
+      const response = await movieApi
+      .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+      console.log("The response from API" ,response);
+    };
+    fetchMovies();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className='banner-img'>
+      <MovieListing />
+    </div>
+    
   )
 }
 
